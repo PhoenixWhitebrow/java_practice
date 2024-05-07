778ea4ce60fdae4f134e54f20b5ca4de5c5548e0
Belsky Mikhail <belsky@jivosite.com>
2024-05-07 20:15:44 +0300

    Added some classes

--- sandbox/src/main/java/my/first/pak/HelloWorld.java
+++ sandbox/src/main/java/my/first/pak/HelloWorld.java
@@ -6,23 +6,25 @@ public class HelloWorld {
 		hello("World");
 		hello("maker");
 		hello("Mikhail");
-		double l = 6;
-		double a = 7;
-		double b = 8;
-		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+		Square s = new Square();
+		s.l = 8;
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+		
+		Rectangle r = new Rectangle(6,7);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
 	}
 
 	public static void hello(String name) {
 		System.out.println("Hello, " + name + "!");
 	}
 
-	public static double area(double l) {
-		return l * l;
+	public static double area(Square s) {
+		return s.l * s.l;
 	}
 
-	public static double area(double a, double b) {
-		return a * b;
+	public static double area(Rectangle r) {
+		return r.a * r.b;
 	}
 
 	public static void mathematicsExample() {
