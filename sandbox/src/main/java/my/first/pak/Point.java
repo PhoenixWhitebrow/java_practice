71b383e186e0e1a0e81e22d21b38a77cf14f29f7
Belsky Mikhail <belsky@jivosite.com>
2024-05-08 09:23:13 +0300

    Added Point and DistanceXY classes, made a distance() function that is calculating distance between points

---
+++ sandbox/src/main/java/my/first/pak/Point.java
@@ -0,0 +1,10 @@
+package my.first.pak;
+
+public class Point {
+    public double x;
+    public double y;
+    public  Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
