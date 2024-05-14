52204bb2a348ba44af829f4d9f21ccb341cf45e3
Belsky Mikhail <belsky@jivosite.com>
2024-05-14 18:58:16 +0300

    Added PointTests

---
+++ sandbox/src/test/java/my/first/pak/PointTetst.java
@@ -0,0 +1,14 @@
+package my.first.pak;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTetst {
+
+    @Test
+    public void testDistance() {
+        Point p1 = new Point(1,2);
+        Point p2 = new Point(1,4);
+        Assert.assertEquals(p1.distance(p2), 2);
+    }
+}
