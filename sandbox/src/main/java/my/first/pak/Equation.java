9e1282d6a2a5e2cefb05946f74b7ae38d78fc773
Belsky Mikhail <belsky@jivosite.com>
2024-05-15 19:12:02 +0300

    Added some condition checks and tests

--- sandbox/src/main/java/my/first/pak/Equation.java
+++ sandbox/src/main/java/my/first/pak/Equation.java
@@ -2,31 +2,41 @@ package my.first.pak;
 
 public class Equation {
 
-    private final double a;
-    private final double b;
-    private final double c;
+  private final double a;
+  private final double b;
+  private final double c;
 
-    private int n;
+  private int n;
 
-    public Equation(double a, double b, double c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
+  public Equation(double a, double b, double c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
 
-        double d = Math.pow(b, 2) - 4 * a * c;
+    double d = Math.pow(b, 2) - 4 * a * c;
 
-        if (d > 0) {
-            n = 2;
+    if (a == 0) {
+      if (b == 0) {
+        if (c == 0) {
+          n = -1;
         } else {
-            if (d == 0) {
-                n = 1;
-            } else {
-                n = 0;
-            }
+          n = 0;
         }
+      } else {
+        n = 1;
+      }
+    } else {
+      if (d > 0) {
+        n = 2;
+      } else if (d == 0) {
+        n = 1;
+      } else {
+        n = 0;
+      }
     }
+  }
 
-    public int rootNumber() {
-        return n;
-    }
+  public int rootNumber() {
+    return n;
+  }
 }
