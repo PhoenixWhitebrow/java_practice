d336d95266687ead39e075ddafc0fd082227c797
Belsky Mikhail <belsky@jivosite.com>
2024-05-20 14:19:54 +0300

    Added if text is null check for the type() method

--- addressbook-web-tests/src/test/java/pw/tests/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/pw/tests/addressbook/appmanager/HelperBase.java
@@ -6,29 +6,31 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public class HelperBase {
-    protected WebDriver driver;
+  protected WebDriver driver;
 
-    public HelperBase(WebDriver driver) {
-        this.driver = driver;
-    }
+  public HelperBase(WebDriver driver) {
+    this.driver = driver;
+  }
 
-    protected void click(By locator) {
-        driver.findElement(locator).click();
-    }
+  protected void click(By locator) {
+    driver.findElement(locator).click();
+  }
 
-    protected void type(By locator, String text) {
-        driver.findElement(locator).click();
-        driver.findElement(locator).clear();
-        driver.findElement(locator).sendKeys(text);
+  protected void type(By locator, String text) {
+    if (text != null) {
+      driver.findElement(locator).click();
+      driver.findElement(locator).clear();
+      driver.findElement(locator).sendKeys(text);
     }
+  }
 
-    protected void select(By locator, String option) {
-        driver.findElement(locator).click();
-        Select dropdown = new Select(driver.findElement(locator));
-        dropdown.selectByValue(option);
-    }
+  protected void select(By locator, String option) {
+    driver.findElement(locator).click();
+    Select dropdown = new Select(driver.findElement(locator));
+    dropdown.selectByValue(option);
+  }
 
-    protected void acceptAlert() {
-        driver.switchTo().alert().accept();
-    }
+  protected void acceptAlert() {
+    driver.switchTo().alert().accept();
+  }
 }
