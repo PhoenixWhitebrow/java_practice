6352ebe04bb8fb48ca1e68436c00def420f9c24f
Belsky Mikhail <belsky@jivosite.com>
2024-05-20 17:02:25 +0300

    Changed timouts

--- addressbook-web-tests/src/test/java/pw/tests/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/pw/tests/addressbook/appmanager/ApplicationManager.java
@@ -6,17 +6,10 @@ import org.openqa.selenium.remote.Browser;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.safari.SafariDriver;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 import java.time.Duration;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.NoSuchElementException;
-import java.util.Objects;
-
 import org.openqa.selenium.Dimension;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsNot.not;
 
 public class ApplicationManager {
 
@@ -47,10 +40,7 @@ public class ApplicationManager {
     js = (JavascriptExecutor) driver;
     Map<String, Object> vars = new HashMap<String, Object>();
     // Timeouts
-    Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
-        .withTimeout(Duration.ofSeconds(30))
-        .pollingEvery(Duration.ofSeconds(5))
-        .ignoring(NoSuchElementException.class);
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
     // Delegation classes initialisation
     sessionHelper = new SessionHelper(driver);
     navigationHelper = new NavigationHelper(driver);
@@ -58,7 +48,8 @@ public class ApplicationManager {
     contactHelper = new ContactHelper(driver);
     // Open addressbook
     driver.get("http://ab.localhost/index.php");
-    //driver.manage().window().setSize(new Dimension(1024, 768));
+    // Window size changer
+    // driver.manage().window().setSize(new Dimension(1024, 768));
     sessionHelper.login("admin", "secret");
   }
 
