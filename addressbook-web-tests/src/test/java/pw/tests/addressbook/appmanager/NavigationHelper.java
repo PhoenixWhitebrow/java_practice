eeb8a3ed35220f09928eb9a26cae59c843ed013e
Belsky Mikhail <belsky@jivosite.com>
2024-05-21 12:52:22 +0300

    Optimized cross-page navigation

--- addressbook-web-tests/src/test/java/pw/tests/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/pw/tests/addressbook/appmanager/NavigationHelper.java
@@ -10,10 +10,20 @@ public class NavigationHelper extends HelperBase{
   }
 
   public void goToGroupPage() {
-    click(By.xpath("//a[contains(text(),'groups')]"));
+    if (isElementPresent(By.tagName("h1"))
+        && driver.findElement(By.tagName("h1")).getText().equals("Groups")
+        && isElementPresent(By.xpath("//input[@name='new']"))) {
+      return;
+    } else {
+      click(By.xpath("//a[contains(text(),'groups')]"));
+    }
   }
 
   public void goToHomePage() {
-    click(By.xpath("//a[contains(text(),'home')]"));
+    if (isElementPresent(By.xpath("//*[@id=\"maintable\"]"))) {
+      return;
+    } else {
+      click(By.xpath("//a[contains(text(),'home')]"));
+    }
   }
 }
