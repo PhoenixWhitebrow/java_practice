2eeddc845931684988ef7fc7f8c2fe667bfd8106
Belsky Mikhail <belsky@jivosite.com>
2024-06-05 16:49:06 +0300

    Added testNonPrime()

--- sandbox/src/test/java/my/first/pak/PrimeTests.java
+++ sandbox/src/test/java/my/first/pak/PrimeTests.java
@@ -6,8 +6,13 @@ import org.testng.annotations.Test;
 public class PrimeTests {
 
   @Test
-  public void testPrimes() {
+  public void testPrime() {
     int n = Integer.MAX_VALUE;
     Assert.assertTrue(Primes.isPrime(n));
+  }
+
+  @Test
+  public void testNonPrime() {
+    Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
   }
 }
